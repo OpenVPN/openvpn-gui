4acafcf3559552edd3ace1d8cdefced673a8006c
Selva Nair <selva.nair@gmail.com>
2022-07-20 21:09:30 -0400

    Parent status and message box windows in the GUI
    - Proper parenting is required for PLAP and cannot
      hurt in general. The parent window in GUI mode
      is the main window. In PLAP it will be the handle
      obtained from LogonUI.
    
    Signed-off-by: Selva Nair <selva.nair@gmail.com>

--- localization.c
+++ localization.c
@@ -365,7 +365,7 @@ LocalizedDialogBoxParam(const UINT dialogId, DLGPROC dialogFunc, const LPARAM pa
     if (resInfo == NULL)
         return -1;
 
-    return DialogBoxIndirectParam(o.hInstance, resInfo, NULL, dialogFunc, param);
+    return DialogBoxIndirectParam(o.hInstance, resInfo, o.hWnd, dialogFunc, param);
 }
 
 
@@ -381,7 +381,7 @@ CreateLocalizedDialogParam(const UINT dialogId, DLGPROC dialogFunc, const LPARAM
     if (resInfo == NULL)
         return NULL;
 
-    return CreateDialogIndirectParam(o.hInstance, resInfo, NULL, dialogFunc, param);
+    return CreateDialogIndirectParam(o.hInstance, resInfo, o.hWnd, dialogFunc, param);
 }
 
 
