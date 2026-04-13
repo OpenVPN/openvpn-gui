305090818fb1a0a9f7cd5db9b2dab244a983b043
Frank Lichtenheld <frank@lichtenheld.com>
2026-04-13 16:46:15 +0200

    GHA: execute cleanup_res_ids.sh
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- res/cleanup_res_ids.sh
+++ res/cleanup_res_ids.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -u
+ret=0
+
+error() {
+    echo "ERR: $1"
+    ret=1
+}
+
 # Check all ids in STRINGTABLE
 # But ignore IDS_NFO_OVPN_STATE_ since those are accessed by offset
 echo "Checking for unused ids"
@@ -7,7 +15,7 @@ for def in $(grep "^#define IDS_" openvpn-gui-res.h \
                  | grep -v IDS_NFO_OVPN_STATE_ \
                  | cut -d' ' -f2 )
 do
-    git grep -q $def *.c plap/*.c || echo "Didn't find $def"
+    git grep -q $def *.c plap/*.c || error "Didn't find usage of $def"
     # Note that ids should be checked manually before removal
     #sed -i -e "/$def/d" openvpn-gui-res.h res/openvpn-gui-res*.rc
 done
@@ -17,6 +25,10 @@ do
     echo "Checking for obsolete translations in $trans"
     for def in $(grep "^ *IDS_" $trans | sed -e 's/^ *//' | cut -d' ' -f1)
     do
-        git grep -q $def openvpn-gui-res.h || echo "$trans: Obsolete translation for $def"
+        git grep -q $def openvpn-gui-res.h \
+            || error "$trans: Obsolete translation for $def"
     done
 done
+
+[ $ret -eq 0 ] || echo "ERR: Errors found"
+exit $ret
